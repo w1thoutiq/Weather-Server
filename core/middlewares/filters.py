a502355b888739fc5dd772368c9d0899385fc8c5
withoutiq <vovapodlubnyy@gmail.com>
2023-05-22 21:40:07 +0300

    Last-PC-Commit

--- core/middlewares/filters.py
+++ core/middlewares/filters.py
@@ -5,11 +5,7 @@ from core.settings import settings
 
 class IsAdmin(BaseFilter):
     def __call__(self, message: Message) -> bool:
-        match message:
-            case message if settings.bots.admin_id == message.from_user.id:
-                return True
-            case message if settings.bots.admin_id != message.from_user.id:
-                return False
-            case _:
-                print('Что-то не так в фильтре')
-                return False
+        if message.from_user.id == settings.bots.admin_id:
+            return True
+        else:
+            return False
